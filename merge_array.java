7cda9a8271528a4873bb760385cac9ae2f557228
Mrinalsharma1 <checkkro1@gmail.com>
2022-02-25 23:52:57 +0530

    few new programsyyya

---
+++ merge_array.java
@@ -0,0 +1,19 @@
+class demo{
+    void abc(){
+        int a =10;
+        int b = 30;
+   
+    }
+}
+public class merge_array {
+    public static void main(String[] args) {
+       
+        demo obj = new demo();
+        System.out.println(obj instanceof demo1);
+        obj.abc();
+        int a=7, b=6;
+         a < b ?
+            System.out.println(10) : System.out.println(20);
+        System.out.println(abc);
+    }
+}
